f58bc747355fc5c3e45381f3133aac596a9bc058
Murilojj <120492616+Murilojj@users.noreply.github.com>
2023-01-25 20:55:46 +0530

    Add files via upload

---
+++ DTU ISS/Javascript/index.js
@@ -0,0 +1,20 @@
+
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    effect: "fade",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
